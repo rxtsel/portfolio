242e799b92a4e297eb5ecf8b573160b3d4d20a8c
Cristhian Melo <rxtsel@hotmail.com>
2023-03-24 17:07:26 -0500

    feat(dev): add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   base: "/",
   site: "https://rxtsel.dev",
+  integrations: [react()]
 });
