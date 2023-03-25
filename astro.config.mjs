caa51f46a14edc1fd67f82a8d27a18e4d5d1a44e
Cristhian Melo <rxtsel@hotmail.com>
2023-03-25 13:30:46 -0500

    feat: add i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   base: "/",
   site: "https://rxtsel.dev",
-  integrations: [react(), sitemap()]
+  integrations: [react(), sitemap(), astroI18next()]
 });
