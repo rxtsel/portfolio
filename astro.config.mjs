6a0053711fa7aeb9161a4deb609c7de811e71eba
Cristhian Melo <rxtsel@hotmail.com>
2023-03-25 12:11:53 -0500

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   base: "/",
   site: "https://rxtsel.dev",
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
