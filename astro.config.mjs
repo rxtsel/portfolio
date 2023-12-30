c062c957320a135a9d375a656de17c1df9dc9922
Cristhian Melo <rxtsel@hotmail.com>
2023-12-29 19:23:13 -0500

    chore: clean proyect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import astroI18next from "astro-i18next";
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/",
-  site: "https://rxtsel.dev",
-  integrations: [sitemap(), astroI18next()],
-  trailingSlash: "never",
-});
+  base: '/',
+  site: 'https://rxtsel.dev',
+  integrations: [sitemap()],
+  trailingSlash: 'never'
+})
