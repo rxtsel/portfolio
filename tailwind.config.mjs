ae683c0c47041418eaa768853d9e24f9c4ae46a5
Cristhian Melo <rxtsel@hotmail.com>
2023-12-29 19:54:16 -0500

    chore: add custom colors to tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        cBlack: '#1A1A1A',
+        cWhite: '#F5F5F5',
+        cAccent: '#838383',
+        cBlue: '#4895EF',
+        cRed: '#D14343'
+      }
+    }
   },
   plugins: []
 }
