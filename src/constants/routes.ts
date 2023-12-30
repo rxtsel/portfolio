d5ee327fcc43503dbfe913e640eaee9053455630
Cristhian Melo <rxtsel@hotmail.com>
2023-12-29 19:46:16 -0500

    feat: add constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,16 @@
+export const ROUTES = {
+  home: '/',
+  top: '/#top',
+  aboutMe: '/#about-me',
+  portfolio: '/#portfolio',
+  contact: '/#contact'
+}
+
+export const SOCIALS = {
+  github: 'https://github.com/rxtsel',
+  linkedin: 'https://www.linkedin.com/in/rxtsel/',
+  twitter: 'https://twitter.com/rxtsel',
+  tiktok: 'https://www.tiktok.com/@rxtsel',
+  whatsapp:
+    'https://api.whatsapp.com/send?phone=573142216604&text=%C2%A1Hola,%20Cristhian!%20%C2%BFC%C3%B3mo%20est%C3%A1s?'
+}
