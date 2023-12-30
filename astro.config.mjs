ccf3d59a905e44a01ab1f19eb594c12c8132c5ad
Cristhian Melo <rxtsel@hotmail.com>
2023-12-29 19:39:09 -0500

    feat(dep): add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   base: '/',
   site: 'https://rxtsel.dev',
-  integrations: [sitemap()],
+  integrations: [sitemap(), tailwind()],
   trailingSlash: 'never'
 })
+
