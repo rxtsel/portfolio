38dfe9347ebfc04bd5e35a9a0762c3b17c3df051
Cristhian Melo <rxtsel@hotmail.com>
2024-01-18 17:13:37 -0500

    feat: add static data

---
+++ src/data/projects.ts
@@ -0,0 +1,10 @@
+import type { iProject } from '@/types/project'
+
+export const projects: iProject[] = [
+  {
+    title: 'Qantto.io',
+    description: 'Landing page for quote services online',
+    url: 'https://qantto.io/',
+    tags: ['Astro', 'TypeScript', 'Tailwind CSS', 'Figma', 'React']
+  }
+]
