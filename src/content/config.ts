98d6add0b4711757abcbf34752bd2bcca9f3feb2
Cristhian Melo <rxtsel@hotmail.com>
2024-01-28 15:08:32 -0500

    feat: add types for blog post

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content'
+
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    draft: z.boolean().default(false),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    categories: z.array(z.string()).default(['all']),
+    tags: z.array(z.string()).default(['all']),
+    authors: z.array(z.string()).default(['rxtsel'])
+  })
+})
+
+export const collections = { blog }
