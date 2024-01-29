e595a9149f8ce4b95d5f9e7ee92a8ef65f77eeb4
Cristhian Melo <rxtsel@hotmail.com>
2024-01-28 19:58:20 -0500

    feat(dep): add mdx plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
   base: '/',
   site: 'https://rxtsel.dev',
-  integrations: [sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind(), mdx()],
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
@@ -16,3 +17,4 @@ export default defineConfig({
   },
   trailingSlash: 'never'
 })
+
