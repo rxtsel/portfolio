7449fcb3deb36f87a9fe325442dfa35dde7e7f1a
Cristhian Melo <rxtsel@hotmail.com>
2024-10-16 18:06:19 -0500

    chore(socials): replace twitter with x

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,7 +1,7 @@
 export const SOCIALS = {
   github: 'https://github.com/rxtsel',
   linkedin: 'https://www.linkedin.com/in/rxtsel/',
-  twitter: 'https://twitter.com/rxtsel',
+  x: 'https://x.com/rxtsel',
   tiktok: 'https://www.tiktok.com/@rxtsel',
   whatsapp:
     'https://api.whatsapp.com/send?phone=573142216604&text=%C2%A1Hola,%20Cristhian!%20%C2%BFC%C3%B3mo%20est%C3%A1s?',
