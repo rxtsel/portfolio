775ee20b341f2bf8fd8b3d28df98558416444edc
Cristhian Melo <rxtsel@hotmail.com>
2024-10-16 17:53:57 -0500

    chore(og): update og template and center name

--- src/pages/[lang]/blog/[slug]/og.png.ts
+++ src/pages/[lang]/blog/[slug]/og.png.ts
@@ -59,7 +59,7 @@ export async function GET({ props }: Props) {
         {
           type: 'div',
           props: {
-            tw: 'absolute right-[40px] bottom-[40px] flex items-center',
+            tw: 'absolute right-[40px] bottom-[50px] flex items-center',
             children: [
               {
                 type: 'div',
