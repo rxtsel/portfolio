7ac7f66eb7022449735c8bf711ad93395689a6fb
Cristhian Melo <rxtsel@hotmail.com>
2024-10-16 17:13:36 -0500

    chore(icons): refactor all icons

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -7,3 +7,5 @@ export const SOCIALS = {
     'https://api.whatsapp.com/send?phone=573142216604&text=%C2%A1Hola,%20Cristhian!%20%C2%BFC%C3%B3mo%20est%C3%A1s?',
   email: 'mailto:contact@rxtsel.dev'
 }
+
+export type SocialsNames = keyof typeof SOCIALS
