d4c2e9457ca623b8265b2d3c8b576821b97a7b36
Cristhian Melo <rxtsel@hotmail.com>
2024-10-16 17:56:08 -0500

    chore(*): fix typos and refactor icons

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,7 +1,7 @@
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 
-export async function GET(context) {
+export async function GET(context: any) {
   const posts = await getCollection('blog')
   return rss({
     title: 'rxtsel.dev',
