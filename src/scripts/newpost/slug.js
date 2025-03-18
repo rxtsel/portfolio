b5cc5c75c696920e6616a2f6a8f50fc828247103
Cristhian Melo <rxtsel@outlook.com>
2025-03-17 21:24:38 -0500

    refactor(slugs): replace manual slugify with slug dep

--- src/scripts/newpost/slug.js
+++ src/scripts/newpost/slug.js
@@ -1,23 +1,10 @@
+import slug from 'slug'
+
 export const slugify = (text) => {
-  return encodeURI(
-    text
-      .replace(/Á/gi, 'a')
-      .replace(/É/gi, 'e')
-      .replace(/Í/gi, 'i')
-      .replace(/Ó/gi, 'o')
-      .replace(/Ú/gi, 'u')
-      .replace(/À/gi, 'a')
-      .replace(/È/gi, 'e')
-      .replace(/Ì/gi, 'i')
-      .replace(/Ò/gi, 'o')
-      .replace(/Ù/gi, 'u')
-      .replace(/ñ/gi, 'n')
-      .replace(/\?/gi, '')
-      .replace(/¿/gi, '')
-      .replace(/!/gi, '')
-      .replace(/¡/gi, '')
-      .replace(/[^a-zA-Z0-9ñ]/g, '-')
-      .replace(/-+/g, '-')
-      .toLowerCase()
-  )
+  return slug(text, {
+    replacement: '-',
+    lower: true,
+    strict: true,
+    trim: true
+  })
 }
