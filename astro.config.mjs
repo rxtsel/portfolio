ca8e39363afdfc8829ba37c8bfc530412edb4120
Cristhian Melo <rxtsel@outlook.com>
2026-05-05 21:51:27 -0500

    feat(seo): add sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,23 @@
 
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
-
 import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://rxtsel.dev",
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en-US",
+          es: "es-CO",
+        },
+      },
+    }),
+  ],
 })
