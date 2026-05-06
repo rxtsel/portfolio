c3fa29e50b3adae6872c8c2523bdafe024c4f798
Cristhian Melo <rxtsel@outlook.com>
2026-05-05 21:47:16 -0500

    chore: update ts and add ts check

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 
+import sitemap from "@astrojs/sitemap"
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 })
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,11 +10,47 @@ export default [
     ignores: ["dist/**", ".astro/**"],
   },
   {
+    rules: {
+      curly: ["error", "all"],
+      eqeqeq: ["error", "always", { null: "ignore" }],
+      "no-alert": "error",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-implicit-coercion": "error",
+      "no-return-await": "error",
+      "no-var": "error",
+      "prefer-const": "error",
+    },
+  },
+  {
     files: ["**/*.astro"],
     languageOptions: {
       parserOptions: {
+        extraFileExtensions: [".astro"],
         parser: tsParser,
+        sourceType: "module",
       },
+    },
+    rules: {
+      "astro/no-set-html-directive": "error",
+      "astro/no-unused-css-selector": "warn",
+      "astro/prefer-class-list-directive": "error",
+      "astro/sort-attributes": [
+        "error",
+        {
+          ignoreCase: true,
+          order: "asc",
+          type: "alphabetical",
+        },
+      ],
+      "no-unused-vars": [
+        "error",
+        {
+          args: "after-used",
+          caughtErrors: "all",
+          ignoreRestSiblings: false,
+          vars: "all",
+        },
+      ],
     },
   },
 ]
