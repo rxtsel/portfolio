881737224f25efc649470c29c07b95c06ee55f38
Cristhian Melo <rxtsel@outlook.com>
2026-05-06 00:50:31 -0500

    feat(i18n): add support for i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,6 +24,13 @@ const seoGraphIntegration = /** @type {import("astro").AstroIntegration} */ (
 // https://astro.build/config
 export default defineConfig({
   site: "https://rxtsel.dev",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "es"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
   },
