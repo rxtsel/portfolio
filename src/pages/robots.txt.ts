b8a71a854c64a114d8d0c6c46f2640838f4b5465
Cristhian Melo <rxtsel@outlook.com>
2026-05-12 18:11:41 -0500

    chore(robots): update robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -3,6 +3,12 @@ import type { APIRoute } from "astro"
 const getRobotsTxt = (sitemapURL: URL) => `\
 User-agent: *
 Allow: /
+Allow: /llms.txt
+Allow: /rss.xml
+Allow: /es/rss.xml
+Allow: /blog/*/og.png
+Allow: /es/blog/*/og.png
+Disallow: /cms-rxtsel/
 
 Sitemap: ${sitemapURL.href}
 `
