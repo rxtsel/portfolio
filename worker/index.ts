6760d3675eac1e3ce4511dd6e05f91806577123b
Cristhian Melo <rxtsel@outlook.com>
2026-05-14 16:07:46 -0500

    chore: remove backend c15t

--- worker/index.ts
+++
@@ -1,8 +0,0 @@
-import { createC15t } from "./c15t"
-import type { Env } from "./env"
-
-export default {
-  async fetch(request: Request, env: Env): Promise<Response> {
-    return createC15t(env).handler(request)
-  },
-}
